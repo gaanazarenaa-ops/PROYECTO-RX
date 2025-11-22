5c555b4dfe8abf72844b0ed82ef3999ec00a6f7f
Naza <gaa.nazarenaa@gmail.com>
2025-11-22 01:15:48 -0300

    Agrego archivos

---
+++ data.js
@@ -0,0 +1,615 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1--presente-l-puerta-ciudadela",
+      "name": "1- PRESENTE l PUERTA CIUDADELA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 0.040170116982613635,
+        "pitch": -0.1251600730844462,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4139937908816087,
+          "pitch": -0.03748381474417428,
+          "rotation": 0,
+          "target": "6-2--presente-l-teatro-sols"
+        },
+        {
+          "yaw": 0.18007546949562325,
+          "pitch": 0.27838998201762166,
+          "rotation": 0,
+          "target": "1-1--pasado-l-puerta-ciudadela"
+        },
+        {
+          "yaw": 0.179733447609026,
+          "pitch": 0.14898778836208315,
+          "rotation": 0,
+          "target": "10-1--futuro-l-puerta-ciudadela"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1--pasado-l-puerta-ciudadela",
+      "name": "1- PASADO l PUERTA CIUDADELA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.028499696577831912,
+        "pitch": -0.1651889017139041,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2591459526957003,
+          "pitch": 0.13873792499446047,
+          "rotation": 0,
+          "target": "0-1--presente-l-puerta-ciudadela"
+        },
+        {
+          "yaw": 0.27319698278238214,
+          "pitch": -0.005552381963600084,
+          "rotation": 0,
+          "target": "10-1--futuro-l-puerta-ciudadela"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2--pasado-l-teatro-sols",
+      "name": "2- PASADO l TEATRO SOLÍS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.0623331875712374,
+        "pitch": -0.1727079257864439,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.17748331086948888,
+          "pitch": 0.05704393405515518,
+          "rotation": 0,
+          "target": "6-2--presente-l-teatro-sols"
+        },
+        {
+          "yaw": 0.17230247891434658,
+          "pitch": 0.18591104756455223,
+          "rotation": 0,
+          "target": "11-2--futuro-l-teatro-sols"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4--pasado-l-palacio-salvo",
+      "name": "4- PASADO l PALACIO SALVO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.885617458209607,
+        "pitch": -0.47996866574944264,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8339368537979492,
+          "pitch": 0.00011714719269662055,
+          "rotation": 0,
+          "target": "12-4--futuro-l-palacio-salvo"
+        },
+        {
+          "yaw": -2.8166434093327695,
+          "pitch": -0.12388740853025659,
+          "rotation": 0,
+          "target": "7-4--presente-l-palacio-salvo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5--pasado-l-monumento-artigas",
+      "name": "5- PASADO l MONUMENTO ARTIGAS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.05342879075499596,
+        "pitch": -0.1727085899299965,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3959363639264968,
+          "pitch": 0.055417306818149825,
+          "rotation": 0,
+          "target": "8-5--presente-l-monumento-artigas"
+        },
+        {
+          "yaw": 0.3988629758189006,
+          "pitch": -0.07609925099577453,
+          "rotation": 0,
+          "target": "13-5--futuro-l-monumento-artigas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6--pasado-l-hotel-radisson",
+      "name": "6- PASADO l HOTEL RADISSON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.434551364782255,
+        "pitch": -0.3429138365914266,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7319056680916134,
+          "pitch": -0.034379039453845905,
+          "rotation": 0,
+          "target": "9-6--presente-l-hotel-radisson"
+        },
+        {
+          "yaw": 0.7249709470438965,
+          "pitch": 0.08825654332101251,
+          "rotation": 0,
+          "target": "14-6--futuro-l-hotel-radisson"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2--presente-l-teatro-sols",
+      "name": "2- PRESENTE l TEATRO SOLÍS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.848296552302947,
+        "pitch": -0.16479905819054075,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.046039689847916,
+          "pitch": 0.22892871844497265,
+          "rotation": 0,
+          "target": "2-2--pasado-l-teatro-sols"
+        },
+        {
+          "yaw": -3.0403163277576297,
+          "pitch": 0.10769998243899792,
+          "rotation": 0,
+          "target": "11-2--futuro-l-teatro-sols"
+        },
+        {
+          "yaw": 0.7980781865802875,
+          "pitch": -0.10842100202562222,
+          "rotation": 0,
+          "target": "7-4--presente-l-palacio-salvo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-4--presente-l-palacio-salvo",
+      "name": "4- PRESENTE l PALACIO SALVO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": -0.897995935149126,
+        "pitch": -0.3471918517787369,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3654782858412684,
+          "pitch": 0.07830279391522055,
+          "rotation": 0,
+          "target": "12-4--futuro-l-palacio-salvo"
+        },
+        {
+          "yaw": 2.8349018047669317,
+          "pitch": -0.3421097298779401,
+          "rotation": 0,
+          "target": "8-5--presente-l-monumento-artigas"
+        },
+        {
+          "yaw": -0.3805020649829203,
+          "pitch": 0.1765823182241757,
+          "rotation": 0,
+          "target": "3-4--pasado-l-palacio-salvo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-5--presente-l-monumento-artigas",
+      "name": "5- PRESENTE l MONUMENTO ARTIGAS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 3.0624045082972735,
+        "pitch": -0.26031415332489516,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7155882320512958,
+          "pitch": 0.022180810433154008,
+          "rotation": 0,
+          "target": "9-6--presente-l-hotel-radisson"
+        },
+        {
+          "yaw": -2.90069326176201,
+          "pitch": -0.08640581258047675,
+          "rotation": 0,
+          "target": "13-5--futuro-l-monumento-artigas"
+        },
+        {
+          "yaw": -2.906364102446334,
+          "pitch": 0.04922651608004358,
+          "rotation": 0,
+          "target": "4-5--pasado-l-monumento-artigas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-6--presente-l-hotel-radisson",
+      "name": "6- PRESENTE l HOTEL RADISSON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 2.977599953981697,
+        "pitch": -0.1797041197271163,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.953781455370459,
+          "pitch": 0.12226594905747312,
+          "rotation": 0,
+          "target": "14-6--futuro-l-hotel-radisson"
+        },
+        {
+          "yaw": -2.9565185782248307,
+          "pitch": 0.256434454709046,
+          "rotation": 0,
+          "target": "5-6--pasado-l-hotel-radisson"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-1--futuro-l-puerta-ciudadela",
+      "name": "1- FUTURO l PUERTA CIUDADELA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.01602853394688708,
+        "pitch": -0.17521144935329502,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3357627119261739,
+          "pitch": -0.10574910751190458,
+          "rotation": 0,
+          "target": "0-1--presente-l-puerta-ciudadela"
+        },
+        {
+          "yaw": 0.3272177465335826,
+          "pitch": 0.029900472275535606,
+          "rotation": 0,
+          "target": "1-1--pasado-l-puerta-ciudadela"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-2--futuro-l-teatro-sols",
+      "name": "2- FUTURO l TEATRO SOLÍS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.10329499654659813,
+        "pitch": -0.20775071851890203,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2843239277249232,
+          "pitch": 0.07838690029671369,
+          "rotation": 0,
+          "target": "2-2--pasado-l-teatro-sols"
+        },
+        {
+          "yaw": 0.2994991403304468,
+          "pitch": -0.048209881992846704,
+          "rotation": 0,
+          "target": "6-2--presente-l-teatro-sols"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-4--futuro-l-palacio-salvo",
+      "name": "4- FUTURO l PALACIO SALVO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.22440242113497177,
+        "pitch": -0.4956255448294442,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10781385259123688,
+          "pitch": -0.09880651958945919,
+          "rotation": 0,
+          "target": "3-4--pasado-l-palacio-salvo"
+        },
+        {
+          "yaw": 0.09041528704730517,
+          "pitch": 0.016302325677894913,
+          "rotation": 0,
+          "target": "3-4--pasado-l-palacio-salvo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-5--futuro-l-monumento-artigas",
+      "name": "5- FUTURO l MONUMENTO ARTIGAS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.10151404833030497,
+        "pitch": -0.16770238723816888,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.471413090975938,
+          "pitch": 0.10064031750476055,
+          "rotation": 0,
+          "target": "4-5--pasado-l-monumento-artigas"
+        },
+        {
+          "yaw": 0.47521030936960074,
+          "pitch": -0.04153444081990543,
+          "rotation": 0,
+          "target": "8-5--presente-l-monumento-artigas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-6--futuro-l-hotel-radisson",
+      "name": "6- FUTURO l HOTEL RADISSON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.03027611967744548,
+        "pitch": -0.2728295240878804,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5546540884295403,
+          "pitch": -0.06017039613145414,
+          "rotation": 0,
+          "target": "9-6--presente-l-hotel-radisson"
+        },
+        {
+          "yaw": 0.542168050702271,
+          "pitch": 0.060665805404150674,
+          "rotation": 0,
+          "target": "5-6--pasado-l-hotel-radisson"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
